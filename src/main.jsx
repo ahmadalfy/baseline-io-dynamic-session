b63f546c3984358abde3cf6ba385fd613e10c50d
Jeremy Wagner <jlwagner@google.com>
2025-03-24 08:34:22 -0500

    First commit of Vite goodness.

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./css/normalize.css"
+import "./css/styles.css"
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    Hey dawgie
+  </StrictMode>
+);
